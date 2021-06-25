651f2a60c61531b74ac6d1b5cc95ea96391fb240
Santhosh <85781259+Santhosh02K@users.noreply.github.com>
2021-06-25 23:10:44 +0700

    Add files via upload

---
+++ ENLIST ASSIGNMENT DAY12.py
@@ -0,0 +1,9 @@
+f = open("30Days30HourOperations.txt", "x")
+
+f = open("30Days30HourOperations.txt", "w")
+f.write("I have completed 10 days successfully")
+f.close()
+
+f = open("30Days30HourOperations.txt", "a")
+f.write("Deira Raj")
+f.close()
